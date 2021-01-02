896ef2fd7addcdf5f1f307d5bed8e58de4e1ee94
thato <thato@DESKTOP-L3DBDFM>
2021-01-01 23:39:24 -0800

    Moved most things to proper interfaces instead of the raw classes, moved search mode around, might Investigations and Police Response might need to be changed, they are weird

---
+++ Los Santos RED/lsr/Debug/Debug.cs
@@ -0,0 +1,385 @@
+﻿using ExtensionsMethods;
+using LosSantosRED.lsr;
+using LosSantosRED.lsr.Interface;
+using Rage;
+using Rage.Native;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class Debug
+{
+    private int PlateIndex;
+    private Vector3 StoredPosition;
+    private PlateTypes PlateTypes;
+    private Mod.World World;
+    private Mod.Player Player;
+    public Debug(PlateTypes plateTypes, Mod.World world, Mod.Player targetable)
+    {
+        PlateTypes = plateTypes;
+        World = world;
+        Player = targetable;
+    }
+
+    public void Update()
+    {
+        if (Game.IsKeyDown(Keys.NumPad0))
+        {
+            DebugNumpad0();
+        }
+        if (Game.IsKeyDown(Keys.NumPad1))
+        {
+            DebugNumpad1();
+        }
+        if (Game.IsKeyDown(Keys.NumPad2))
+        {
+            DebugNumpad2();
+        }
+        if (Game.IsKeyDown(Keys.NumPad3))
+        {
+            DebugNumpad3();
+        }
+        if (Game.IsKeyDown(Keys.NumPad4))
+        {
+            DebugNumpad4();
+        }
+        if (Game.IsKeyDown(Keys.NumPad5))
+        {
+            DebugNumpad5();
+        }
+        if (Game.IsKeyDown(Keys.NumPad6))
+        {
+            DebugNumpad6();
+        }
+        if (Game.IsKeyDown(Keys.NumPad7))
+        {
+            DebugNumpad7();
+        }
+        if (Game.IsKeyDown(Keys.NumPad8))
+        {
+            DebugNumpad8();
+        }
+        if (Game.IsKeyDown(Keys.NumPad9))
+        {
+            DebugNumpad9();
+        }
+
+
+        //Vector3 Position = NativeFunction.CallByName<Vector3>("GET_WORLD_POSITION_OF_ENTITY_BONE", Game.LocalPlayer.Character, NativeFunction.CallByName<int>("GET_PED_BONE_INDEX", Game.LocalPlayer.Character, 31086));
+        //Rage.Debug.DrawArrowDebug(Position, Vector3.Zero, Rotator.Zero, 1f, Color.White);
+        //Vector3 Position2 = NativeFunction.CallByName<Vector3>("GET_WORLD_POSITION_OF_ENTITY_BONE", Game.LocalPlayer.Character, NativeFunction.CallByName<int>("GET_PED_BONE_INDEX", Game.LocalPlayer.Character, 57005));
+        //Rage.Debug.DrawArrowDebug(Position2, Vector3.Zero, Rotator.Zero, 1f, Color.Red);
+
+
+    }
+    private void DebugNumpad0()
+    {
+        DebugNonInvincible();
+    }
+    private void DebugNonInvincible()
+    {
+        Game.LocalPlayer.Character.IsInvincible = false;
+        Game.LocalPlayer.Character.Health = Game.LocalPlayer.Character.MaxHealth;
+        //Game.Console.Print("KeyDown: You are NOT invicible");
+    }
+    private void DebugNumpad1()
+    {
+        DebugInvincible();
+    }
+    private void DebugInvincible()
+    {
+        Game.LocalPlayer.Character.IsInvincible = true;
+        Game.LocalPlayer.Character.Health = Game.LocalPlayer.Character.MaxHealth;
+        //Game.Console.Print("KeyDown: You are invicible");
+    }
+    private void DebugNumpad2()
+    {
+        PlateIndex--;
+        SetIndex();
+    }
+    public void LoadNorthYankton()
+    {
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_01");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01c");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01c_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01d");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01d_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01e");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01e_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01f");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01f_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01g");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01h");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01h_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01i");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01i_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01j");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01j_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01k");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01k_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01z");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue01z_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_02");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue02");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue02_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_03");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue03");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue03_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue03b");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue03b_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue03_grv_dug");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue03_grv_dug_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue_grv_torch");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_04");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue04");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue04_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue04b");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue04b_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue04_cover");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "des_protree_end");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "des_protree_start");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "des_protree_start_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_05");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue05");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue05_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue05b");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue05b_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_06");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06b");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06b_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06_int");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06_int_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06_pannel");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue06_pannel_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue_m2_door");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue_m2_door_lod");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_occl_00");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologue_occl");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "plg_rd");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologuerd");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologuerdb");
+        NativeFunction.CallByName<bool>("REQUEST_IPL", "prologuerd_lod");
+    }
+
+    private void DebugNumpad3()
+    {
+        PlateIndex++;
+        SetIndex();
+    }
+    public void UnLoadNorthYankton()
+    {
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_01");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01c");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01c_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01d");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01d_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01e");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01e_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01f");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01f_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01g");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01h");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01h_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01i");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01i_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01j");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01j_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01k");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01k_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01z");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue01z_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_02");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue02");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue02_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_03");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue03");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue03_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue03b");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue03b_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue03_grv_dug");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue03_grv_dug_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue_grv_torch");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_04");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue04");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue04_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue04b");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue04b_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue04_cover");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "des_protree_end");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "des_protree_start");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "des_protree_start_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_05");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue05");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue05_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue05b");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue05b_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_06");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06b");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06b_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06_int");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06_int_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06_pannel");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue06_pannel_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue_m2_door");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue_m2_door_lod");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_occl_00");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologue_occl");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "plg_rd");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologuerd");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologuerdb");
+        NativeFunction.CallByName<bool>("REMOVE_IPL", "prologuerd_lod");
+    }
+    private void SetIndex()
+    {
+        if (PlateIndex < 0)
+        {
+            return;
+        }
+        if (Game.LocalPlayer.Character.IsInAnyVehicle(false))
+        {
+            Vehicle Car = Game.LocalPlayer.Character.CurrentVehicle;
+            PlateType NewType = PlateTypes.GetPlateType(PlateIndex);
+            if (NewType != null)
+            {
+                string NewPlateNumber = NewType.GenerateNewLicensePlateNumber();
+                if (NewPlateNumber != "")
+                {
+                    Car.LicensePlate = NewPlateNumber;
+                }
+                NativeFunction.CallByName<int>("SET_VEHICLE_NUMBER_PLATE_TEXT_INDEX", Car, NewType.Index);
+                Game.DisplaySubtitle($" PlateIndex: {PlateIndex}, Index: {NewType.Index}, State: {NewType.State}, Description: {NewType.Description}");
+            }
+            else
+            {
+                Game.DisplaySubtitle($" PlateIndex: {PlateIndex} None Found");
+            }
+        }
+    }
+    private void DebugNumpad4()
+    {
+        //Game.Console.Print($"Position {Game.LocalPlayer.Character.Position}");
+        //Game.Console.Print($"Zone {GetInternalZoneString(Game.LocalPlayer.Character.Position)}");
+    }
+
+    private string GetInternalZoneString(Vector3 ZonePosition)
+    {
+        string zoneName;
+        unsafe
+        {
+            IntPtr ptr = Rage.Native.NativeFunction.CallByName<IntPtr>("GET_NAME_OF_ZONE", ZonePosition.X, ZonePosition.Y, ZonePosition.Z);
+
+            zoneName = Marshal.PtrToStringAnsi(ptr);
+        }
+        return zoneName;
+    }
+    private void DebugNumpad5()
+    {
+        NativeFunction.CallByName<bool>("SET_PED_IS_DRUNK", Game.LocalPlayer.Character, true);
+        if (!NativeFunction.CallByName<bool>("HAS_ANIM_SET_LOADED", "move_m@drunk@verydrunk"))
+        {
+            NativeFunction.CallByName<bool>("REQUEST_ANIM_SET", "move_m@drunk@verydrunk");
+        }
+        NativeFunction.CallByName<bool>("SET_PED_MOVEMENT_CLIPSET", Game.LocalPlayer.Character, "move_m@drunk@verydrunk", 0x3E800000);
+        NativeFunction.CallByName<bool>("SET_PED_CONFIG_FLAG", Game.LocalPlayer.Character, (int)PedConfigFlags.PED_FLAG_DRUNK, true);
+        NativeFunction.CallByName<int>("SET_TIMECYCLE_MODIFIER", "Drunk");
+        NativeFunction.CallByName<int>("SET_TIMECYCLE_MODIFIER_STRENGTH", 1.1f);
+        NativeFunction.Natives.x80C8B1846639BB19(1);
+        NativeFunction.CallByName<int>("SHAKE_GAMEPLAY_CAM", "DRUNK_SHAKE", 5.0f);
+        //Game.Console.Print("Player Made Drunk");
+    }
+    private void DebugNumpad6()
+    {
+        NativeFunction.CallByName<bool>("SET_PED_IS_DRUNK", Game.LocalPlayer.Character, false);
+        NativeFunction.CallByName<bool>("RESET_PED_MOVEMENT_CLIPSET", Game.LocalPlayer.Character);
+        NativeFunction.CallByName<bool>("SET_PED_CONFIG_FLAG", Game.LocalPlayer.Character, (int)PedConfigFlags.PED_FLAG_DRUNK, false);
+        NativeFunction.CallByName<int>("CLEAR_TIMECYCLE_MODIFIER");
+        NativeFunction.Natives.x80C8B1846639BB19(0);
+
+        NativeFunction.CallByName<int>("STOP_GAMEPLAY_CAM_SHAKING", true);
+
+        //Game.Console.Print("Player Made Sober");
+    }
+    private void DebugNumpad7()
+    {
+        foreach(Cop cop in World.PoliceList)
+        {
+            cop.CurrentTask = new Chase(cop, Player);
+        }
+
+
+    }
+    public void DebugNumpad8()
+    {
+        Ped newped = new Ped("s_m_y_cop_01", Game.LocalPlayer.Character.GetOffsetPositionFront(2f), Game.LocalPlayer.Character.Heading);
+        Vehicle car = new Vehicle("police", Game.LocalPlayer.Character.GetOffsetPositionFront(-8f), Game.LocalPlayer.Character.Heading);
+        if (newped.Exists() && car.Exists())
+        {
+            newped.WarpIntoVehicle(car, -1);
+            GameFiber.StartNew(delegate
+            {
+                try
+                {
+                    while (newped.Exists() && newped.IsAlive && !Game.IsKeyDownRightNow(Keys.E))
+                    {
+                        Game.DisplayHelp("Press E to delete chaser ~n~Press R to Repair");
+                        if (Game.IsKeyDownRightNow(Keys.R) && car.Exists())
+                        {
+                            car.Repair();
+                            newped.Health = newped.MaxHealth;
+                        }
+                        GameFiber.Yield();
+                    }
+                    if (newped.Exists())
+                    {
+                        newped.Delete();
+                    }
+                    if (car.Exists())
+                    {
+                        car.Delete();
+                    }
+                }
+                catch (Exception e)
+                {
+                    if (newped.Exists())
+                    {
+                        newped.Delete();
+                    }
+                    if (car.Exists())
+                    {
+                        car.Delete();
+                    }
+                    //Game.Console.Print("Error" + e.Message + " : " + e.StackTrace);
+                }
+            }, "DebugLoop2");
+
+        }
+    }
+    private void DebugNumpad9()
+    {
+        //Game.Console.Print("Debugging: Pressed Num9");
+        EntryPoint.ModController.Dispose();
+        Game.LocalPlayer.WantedLevel = 0;
+        Game.TimeScale = 1f;
+        NativeFunction.Natives.xB4EDDC19532BFB85();
+        Game.DisplayNotification("Instant Action Deactivated");
+
+
+       
+
+    }
+
+}
+
