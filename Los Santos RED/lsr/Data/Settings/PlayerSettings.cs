e40cb2589b5e697e497996c27a19b73581c0d7cc
thato <thato@DESKTOP-L3DBDFM>
2021-10-15 10:51:38 -0700

    Settings tweak

--- Los Santos RED/lsr/Data/Settings/PlayerSettings.cs
+++ Los Santos RED/lsr/Data/Settings/PlayerSettings.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 public class PlayerSettings
 {
     public string AutoTuneRadioStation { get; set; } = "RADIO_19_USER";
-    public bool KeepRadioStationAutoTuned { get; set; } = true;
+    public bool KeepRadioStationAutoTuned { get; set; } = false;
     public uint Recognize_BaseTime { get; set; } = 2000;
     public uint Recognize_NightPenalty { get; set; } = 3500;
     public uint Recognize_VehiclePenalty { get; set; } = 750;
