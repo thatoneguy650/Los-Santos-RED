83d30fe1fc0277b573580637ed847f8f8fd737a1
peeppeep-io <atillathegunlsrp@gmail.com>
2025-01-24 10:41:03 +0530

    Minor bug fix to filter out valid crafts.

--- Los Santos RED/lsr/Data/Items/CraftableItems.cs
+++ Los Santos RED/lsr/Data/Items/CraftableItems.cs
@@ -40,7 +40,7 @@ public class CraftableItems : ICraftableItems
             EntryPoint.WriteToConsole($"No Craftable Items config found, creating default", 0);
             DefaultConfig();
         }
-        CraftableList.RemoveAll(x => ModItems.Get(x.Resultant) == null);
+        CraftableList.RemoveAll(x => ModItems.Get(x.Resultant) == null || (x.SingleUnit == false && x.ResultantAmount <1));
     }
 
     private void DefaultConfig()
