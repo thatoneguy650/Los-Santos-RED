a684c328c783a4d11eafa04e9cbced30e8e86740
thatoneguy650 <thatoneguy650@gmail.com>
2025-04-06 00:24:22 -0700

    race for the heisman

---
+++ Los Santos RED/lsr/UI/Menu/Debug/Sub Menus/DebugVehicleRaceSubMenu.cs
@@ -0,0 +1,49 @@
+﻿using LosSantosRED.lsr.Interface;
+using Rage;
+using RAGENativeUI;
+using RAGENativeUI.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class DebugVehicleRaceSubMenu : DebugSubMenu
+{
+    private List<VehicleRace> PossibleRaces = new List<VehicleRace>();
+    private IVehicleRaces VehicleRaces;
+    public DebugVehicleRaceSubMenu(UIMenu debug, MenuPool menuPool, IActionable player, IVehicleRaces vehicleRaces) : base(debug, menuPool, player)
+    {
+        VehicleRaces = vehicleRaces;
+    }
+
+    public override void AddItems()
+    {
+        SubMenu = MenuPool.AddSubMenu(Debug, "Racing Menu");
+        Debug.MenuItems[Debug.MenuItems.Count() - 1].Description = "Do vehicle racing";
+        SubMenu.SetBannerType(EntryPoint.LSRedColor);
+        CreateMenu();
+    }
+    public override void Update()
+    {
+
+    }
+    private void CreateMenu()
+    {
+        SubMenu.Clear();
+        foreach(VehicleRaceTrack vrt in VehicleRaces.VehicleRaceTypeManager.VehicleRaceTracks)
+        {
+            UIMenuItem generalOne = new UIMenuItem(vrt.Name, "Start a the specific race.");
+            generalOne.Activated += (sender, selectedItem) =>
+            {
+                VehicleRace vr = new VehicleRace(vrt.Name, vrt);
+                Player.RacingManager.StartDebugRace(vr);
+
+                sender.Visible = false;
+            };
+            SubMenu.AddItem(generalOne);
+        }
+    }
+}
+
