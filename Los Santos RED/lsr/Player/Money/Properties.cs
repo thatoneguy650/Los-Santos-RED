18e6dcf02ac37368afb8c2eb18f50459386831e1
peeppeep-io <atillathegunlsrp@gmail.com>
2025-06-21 18:47:02 +0530

    Removed unwanted logs

--- Los Santos RED/lsr/Player/Money/Properties.cs
+++ Los Santos RED/lsr/Player/Money/Properties.cs
@@ -32,7 +32,6 @@ public class Properties
         //int businessesPayingOut = 0;
         foreach (GameLocation property in PropertyList)
         {
-            EntryPoint.WriteToConsole($"Handling property: {property.FullName}");
             property.HandleOwnedLocation(Player, Time);
         }
     }
